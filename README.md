d42b0cb3b0dd792b745710f1dc1d83d66dc56f7d
BryghtShadow <bryghtshadow@gmail.com>
2020-04-16 19:38:38 +1200

    add tool and requirements; update README

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# xapktool
+# XAPK creation tool
+
+This XAPK creation tool will create an XAPK file from a folder containing an APK file and an OBB file. The `icon.png` and `manifest.json` files are generated from the APK file.
+
+## Usage
+
+```
+xapktool.py <apk_obb_directory>
+```
+
+## Notes
+
+* The tool expects a directory with a single apk and single obb file.
+* There is no check to ensure that the OBB and APK are from the same application. There is also no check for cases where there is no obb file.
+** Probably should enforce that obb files are correctly named (`[main|patch].<expansion-version>.<package-name>.obb`), as per https://developer.android.com/google/play/expansion-files#GettingFilenames
+
+## Authors
+
+* XAPK creation tool: Shii Kayano
+* Androguard + tools: Anthony Desnos (desnos at t0t0.fr).
+* DAD (DAD is A Decompiler): Geoffroy Gueguen (geoffroy dot gueguen at gmail dot com)
+
+## Licenses
+
+### XAPK creation tool
+
+Copyright (C) 2020, Shii Kayano (bryghtshadow at gmail dot com)
+All rights reserved
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS-IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+### Androguard
+
+Copyright (C) 2012 - 2016, Anthony Desnos (desnos at t0t0.fr)
+All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS-IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+### DAD
+
+Copyright (C) 2012 - 2016, Geoffroy Gueguen (geoffroy dot gueguen at gmail dot com)
+All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS-IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ xapk.py
@@ -0,0 +1,112 @@
+import os
+import sys
+import json
+from pathlib import Path, PurePath
+import tempfile
+import shutil
+import zipfile
+
+import androguard
+from androguard.core.bytecodes.apk import APK
+
+class XAPK:
+    def __init__(self, folder):
+        self.folder = Path(folder)
+        for x in self.folder.glob('*.apk'):
+            self.apk_src = Path(x)
+            break
+        for x in self.folder.glob('*.obb'):
+            self.obb_src = Path(x)
+            break
+        self.apk = APK(self.apk_src)
+        self.manifest = self.make_manifest()
+        self.icon = self.apk.get_file(self.apk.get_app_icon())
+
+    def make_manifest(self):
+        apk_size = self.apk_src.stat().st_size
+        if self.obb_src:
+            obb_size = self.obb_src.stat().st_size
+        else:
+            obb_size = 0
+        total_size = apk_size + obb_size
+        filename = self.apk.get_filename()
+
+        manifest = {}
+        manifest['xapk_version'] = 1
+        manifest['package_name'] = self.apk.get_package()
+        manifest['name'] = self.apk.get_app_name()
+        # manifest['locales_name'] = {} # TODO
+        manifest['version_code'] = self.apk.get_androidversion_code()
+        manifest['version_name'] = self.apk.get_androidversion_name()
+        manifest['min_sdk_version'] = self.apk.get_min_sdk_version()
+        manifest['target_sdk_version'] = self.apk.get_target_sdk_version()
+        manifest['permissions'] = self.apk.get_declared_permissions()
+        manifest['total_size'] = total_size
+        manifest['expansions'] = []
+
+        if obb_size:
+            main_obb = {}
+            main_obb['file'] = 'Android/obb/{package_name}/main.{version_code}.{package_name}.obb'.format(**manifest)
+            main_obb['install_location'] = 'EXTERNAL_STORAGE'
+            main_obb['install_path'] = 'Android/obb/{package_name}/main.{version_code}.{package_name}.obb'.format(**manifest)
+            manifest['expansions'].push(main_obb)
+
+        return manifest
+
+    def save(self):
+        self.name = '{package_name}_v{version_name}.xapk'.format(**self.manifest)
+        zip_path = self.folder.joinpath(self.name)
+
+        zip_dir = tempfile.mkdtemp()
+        try:
+            print('copying apk to temp directory...')
+            apk_name = '{package_name}.apk'.format(**self.manifest)
+            apk_src = self.apk_src.resolve()
+            apk_dest = PurePath(zip_dir).joinpath(apk_name)
+            shutil.copy2(apk_src, apk_dest)
+            print('apk: OK')
+
+            if self.manifest.get('expansions'):
+                print('copying obb to temp directory...')
+                obb_name = self.manifest['expansions'][0]['install_path']
+                obb_src = self.obb_src.resolve()
+                obb_dest = PurePath(zip_dir).joinpath(obb_name)
+                os.makedirs(Path(obb_dest).parent, exist_ok=True)
+                shutil.copy2(obb_src, obb_dest)
+                print('obb: OK')
+            else:
+                print('no obb found')
+
+            print('creating icon in temp directory...')
+            icon = self.icon
+            icon_dest = PurePath(zip_dir).joinpath('icon.png')
+            with open(icon_dest, 'wb') as iconfile:
+                iconfile.write(icon)
+            print('icon: OK')
+
+            print('creating manifest in temp directory...')
+            manifest_dest = PurePath(zip_dir).joinpath('manifest.json')
+            with open(manifest_dest, 'w') as manifestfile:
+                s = json.dumps(self.manifest, separators=(':',','))
+                manifestfile.write(s)
+            print('manifest: OK')
+
+            print('creating xapk archive...')
+            with zipfile.ZipFile(zip_path, 'w', compression=zipfile.ZIP_DEFLATED) as zfd:
+                for root, dirs, files in os.walk(zip_dir):
+                    for f in files:
+                        filename = os.path.join(root, f)
+                        zfd.write(filename, os.path.relpath(filename, zip_dir))
+            print('xapk: OK')
+        finally:
+            print('cleaning up temp directory...')
+            shutil.rmtree(zip_dir)
+            print('cleanup: OK')
+
+def main(args):
+    folder = args[0]
+    xapk = XAPK(folder)
+    xapk.save()
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
